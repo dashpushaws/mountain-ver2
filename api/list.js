9e4cf6c98707c885d0f7c84543b43ef36e6be18d
dashpushaws <dashpush.aws@gmail.com>
2021-02-25 17:23:57 +0900

    2.25

--- api/list.js
+++ api/list.js
@@ -5,7 +5,7 @@ export default {
   // GET http:// ...:3000/list
   list: () => axios.get(`${baseUrl}/mtnList`),
   // GET http:// ...:3000/list/:id
-  get: (id) => axios.get(`${baseUrl}/list/${id}`),
+  get: (id) => axios.get(`${baseUrl}/mtnList/${id}`),
   // GET http:// ...:3000/list?q=kewyword
-  search: (keyword) => axios.get(`${baseUrl}/list?q=${keyword}`),
+  search: (keyword) => axios.get(`${baseUrl}/mtnList?q=${keyword}`),
 }
